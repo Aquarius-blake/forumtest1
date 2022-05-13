9c7567d17959ae22eb621748673520a634722553
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-13 14:23:40 +0000

    Pages

--- lib/Assets/Colors.dart
+++ lib/Assets/Colors.dart
@@ -10,9 +10,9 @@ class AppColorsTheme {
   AppColorsTheme({required this.primarySwatch,required this.titleBarGradientStartColor,required this.titleBarGradientEndColor,required this.textColor,required this.secondaryGradientColor});
 
   static final AppColorsTheme myTheme = new AppColorsTheme(
-      primarySwatch: Colors.deepPurple,
-      titleBarGradientStartColor : Colors.deepPurple,
-      titleBarGradientEndColor: Colors.deepPurple,
+      primarySwatch: Colors.lightBlue,
+      titleBarGradientStartColor : Colors.lightBlue,
+      titleBarGradientEndColor: Colors.lightBlue,
       textColor: Colors.black,
       secondaryGradientColor: Colors.grey,
 
