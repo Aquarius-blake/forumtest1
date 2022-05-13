c6f8c90d99a69982d8512ba90dc7609ba87c1853
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-13 14:31:30 +0000

    Pages

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -15,79 +15,81 @@ class LoginPage extends StatelessWidget {
       body: new Container(
         color: Colors.lightBlueAccent,
         padding: const EdgeInsets.all(30.0),
-        child: new Column(
-          children: <Widget>[
-            new Container(
-              margin: const EdgeInsets.all(20.0),
-              decoration: new BoxDecoration(
-                borderRadius: new BorderRadius.all(new Radius.circular(30.0)),
+        child: SingleChildScrollView(
+          child: new Column(
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.all(20.0),
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.all(new Radius.circular(30.0)),
+                ),
+                height: 200.0,
+                alignment: Alignment.center,
+                child: new Icon(
+                  Icons.library_books,
+                  size: 100.0,
+                  color: Colors.white,
+                ),
               ),
-              height: 200.0,
-              alignment: Alignment.center,
-              child: new Icon(
-                Icons.library_books,
-                size: 100.0,
-                color: Colors.white,
+              new TextField(
+                controller: new TextEditingController(),
+                decoration: new InputDecoration(
+                  hintText: 'Username',
+                  filled: true,
+                  fillColor: Colors.white70,
+                ),
               ),
-            ),
-            new TextField(
-              controller: new TextEditingController(),
-              decoration: new InputDecoration(
-                hintText: 'Username',
-                filled: true,
-                fillColor: Colors.white70,
+              new TextField(
+                controller: new TextEditingController(),
+                obscureText: true,
+                decoration: new InputDecoration(
+                  hintText: 'Password',
+                  filled: true,
+                  fillColor: Colors.white70,
+                ),
               ),
-            ),
-            new TextField(
-              controller: new TextEditingController(),
-              obscureText: true,
-              decoration: new InputDecoration(
-                hintText: 'Password',
-                filled: true,
-                fillColor: Colors.white70,
-              ),
-            ),
-            new Container(
-              // margin: const EdgeInsets.only( bottom: 150.0),
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  new Container(
-                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: new ElevatedButton(
-                      child: new Text("Login"),
-                      onPressed: onPressed,
+              new Container(
+                // margin: const EdgeInsets.only( bottom: 150.0),
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    new Container(
+                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: new ElevatedButton(
+                        child: new Text("Login"),
+                        onPressed: onPressed,
 
+                      ),
                     ),
-                  ),
-                  new ElevatedButton(
-                    child: new Text("Forgot Password"),
-                    onPressed: onPressed,
-                  ),
-                ],
-              ),
-            ),
-            new Expanded(
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  new Container(
-                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: new ElevatedButton(
-                      child: new Text("Register"),
+                    new ElevatedButton(
+                      child: new Text("Forgot Password"),
                       onPressed: onPressed,
-
                     ),
-                  ),
-                  new ElevatedButton(
-                    child: new Text("Continue as Guest"),
-                    onPressed: () {Navigator.pushNamed(context, '/forum');},
-                  ),
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
+              new Expanded(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    new Container(
+                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: new ElevatedButton(
+                        child: new Text("Register"),
+                        onPressed: onPressed,
+
+                      ),
+                    ),
+                    new ElevatedButton(
+                      child: new Text("Continue as Guest"),
+                      onPressed: () {Navigator.pushNamed(context, '/forum');},
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
