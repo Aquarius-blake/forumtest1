9c453171c648770afba459ac77f73c6be1c14733
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-13 13:29:56 +0000

    Initializing

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+
+void main(){}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+
+    );
+  }
+}
